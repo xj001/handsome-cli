f029bc93a65511c9723caa67a165064b3c433453
xujun <xj951210@126.com>
2021-04-28 17:18:55 +0800

    空项目初始化git、npm以及typescript

--- src/main.js
+++ src/main.js
@@ -24,12 +24,47 @@ program
                 type: 'list',
                 name: 'template',
                 message: '选择一个模版',
-                choices: choices,
+                choices: ['新建一个项目'].concat(choices),
               },
             ])
             .then((answers) => {
-              child_process.spawn('cp', ['-r', path.join(__dirname, `./template/${answers.template}`), process.cwd()]);
-              console.log(chalk.green('操作成功'));
+                if (answers.template !== "新建一个项目") {
+                    child_process.spawn('cp', ['-r', path.join(__dirname, `./template/${answers.template}`), process.cwd()]);
+                }else{
+                    inquirer.prompt([
+                        {
+                            type:'checkbox',
+                            name:'initFiles',
+                            choices:['npm','git','pretty','typescript']
+                        }
+                    ]).then((answers) => {
+                        let array = answers.initFiles;
+                        fs.mkdirSync(`${process.cwd()}/${name}`);
+                        if(array.indexOf('npm') > -1){
+                            const npm = child_process.spawn(`npm`,['init','-y'],{ cwd: `${process.cwd()}/${name}` });
+                            npm.on('error',(err)=>{
+                                console.log(chalk.red(err));
+                            })
+                        }
+                        if (array.indexOf('git') > -1) {
+                            const git = child_process.spawn(`git`, ['init'], { cwd: `${process.cwd()}/${name}` });
+                            git.on('error', (err) => {
+                              console.log(chalk.red(err));
+                            });
+                        }
+                        if (array.indexOf('typescript') > -1) {
+                          const typescript = child_process.spawn(`tsc`, ['--init'], { cwd: `${process.cwd()}/${name}` });
+                            typescript.on('error', (err) => {
+                              console.log(chalk.red(err));
+                            });
+                        }
+                        child_process.spawn(`open`, ['.'], { cwd: `${process.cwd()}` });
+                        console.log(answers);
+                    }).catch((err) =>{
+                        console.log(chalk.red(err));
+                    })
+                }
+                console.log(chalk.green('操作成功'));
             })
             .catch((error) => {
               if (error.isTtyError) {
